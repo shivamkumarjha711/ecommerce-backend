75e5a7a15c2ab88ef28dcc6707480bcd15e65a64
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-12-06 21:19:50 +0530

    Handling Uncaught Expectation error and Wrong Mongodb id error

--- middleware/error.js
+++ middleware/error.js
@@ -5,6 +5,12 @@ export const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error"
 
+    // Wrong Mongodb id error
+    if (err.name === "Error") {     // CastError
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400)
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
--- server.js
+++ server.js
@@ -2,7 +2,18 @@ import app from "./app.js";
 import connectDb from "./config/db.js";
 
 
+// Handling Uncaught Expectation
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught Expectation");
+    process.exit(1);
+})
+
+
 app.listen(process.env.PORT, async () => {
     await connectDb();              // Database Connection
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 })
+
+
+// Unhandled Promise Rejection
